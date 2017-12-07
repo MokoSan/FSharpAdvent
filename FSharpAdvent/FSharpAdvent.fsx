46d6e75833d2a603492691f2b5c78ef5f11b0c72
Mukund Raghav Sharma <mukund.sharma92@gmail.com>
2017-12-06 21:31:23 -0500

    Add project files.

---
+++ FSharpAdvent/FSharpAdvent.fsx
@@ -0,0 +1,103 @@
+﻿#r @"C:\Users\MukundRaghavSharma\Desktop\F#\FSharpAdvent\packages\FSharp.Data.2.4.3\lib\net45\FSharp.Data.dll"
+
+open FSharp.Data
+
+[<Literal>]
+let baseWikiURL  = @"http://lotr.wikia.com/" 
+
+[<Literal>]
+let characterURLPage1 = @"http://lotr.wikia.com/wiki/Category:Characters" 
+[<Literal>]
+let characterURLPage2 = @"http://lotr.wikia.com/wiki/Category:Characters?page=2" 
+[<Literal>]
+let characterURLPage3 = @"http://lotr.wikia.com/wiki/Category:Characters?page=3" 
+[<Literal>]
+let characterURLPage4 = @"http://lotr.wikia.com/wiki/Category:Characters?page=4" 
+[<Literal>]
+let characterURLPage5 = @"http://lotr.wikia.com/wiki/Category:Characters?page=5" 
+
+[<Literal>]
+let raceURL      = @"http://lotr.wikia.com/wiki/Category:Races"
+
+type LotrCharacterProviderPage1 = HtmlProvider< characterURLPage1 >
+let lotrCharacterProviderPage1  = LotrCharacterProviderPage1.Load( characterURLPage1 )
+
+type LotrCharacterProviderPage2 = HtmlProvider< characterURLPage2 >
+let lotrCharacterProviderPage2  = LotrCharacterProviderPage2.Load( characterURLPage2 )
+
+type LotrCharacterProviderPage3 = HtmlProvider< characterURLPage3 >
+let lotrCharacterProviderPage3  = LotrCharacterProviderPage3.Load( characterURLPage3 )
+
+type LotrCharacterProviderPage4 = HtmlProvider< characterURLPage4 >
+let lotrCharacterProviderPage4  = LotrCharacterProviderPage4.Load( characterURLPage4 )
+
+type LotrCharacterProviderPage5 = HtmlProvider< characterURLPage5 >
+let lotrCharacterProviderPage5  = LotrCharacterProviderPage5.Load( characterURLPage5 )
+
+let getAnchorTagDecendants ( lists : HtmlNode list ) : string list list = 
+    lists
+    |> List.map( fun l -> 
+        Seq.toList ( l.Descendants[ "a" ]  )
+        |> List.map( fun ll -> baseWikiURL + ll.TryGetAttribute( "href").Value.Value() ))
+    |> Seq.toList
+
+ let getCharacterURLsFromAppropriateHtmlNodes ( attributes : seq<HtmlNode> ) : seq<string> =
+    attributes
+    |> Seq.map( fun a -> baseWikiURL + a.TryGetAttribute( "href" ).Value.Value() )
+
+let validCharacterLists =
+    [
+        // Page 1
+        lotrCharacterProviderPage1.Lists.A.Html;
+
+        lotrCharacterProviderPage1.Lists.``A cont.``.Html;
+        lotrCharacterProviderPage1.Lists.B.Html;
+        lotrCharacterProviderPage1.Lists.``B cont.``.Html;
+
+        // Page 2
+        lotrCharacterProviderPage2.Lists.B.Html;
+        lotrCharacterProviderPage2.Lists.C2.Html;
+        lotrCharacterProviderPage2.Lists.``C cont. 2``.Html;
+        lotrCharacterProviderPage2.Lists.D.Html;
+        lotrCharacterProviderPage2.Lists.E.Html;
+        lotrCharacterProviderPage2.Lists.``E cont.``.Html;
+        lotrCharacterProviderPage2.Lists.F.Html;
+
+        // Page 3
+        lotrCharacterProviderPage3.Lists.F.Html;
+        lotrCharacterProviderPage3.Lists.G.Html;
+        lotrCharacterProviderPage3.Lists.``G cont.``.Html;
+        lotrCharacterProviderPage3.Lists.H.Html;
+        lotrCharacterProviderPage3.Lists.``H cont.``.Html;
+        lotrCharacterProviderPage3.Lists.I.Html;
+        // Missing K on Page 3
+
+        // Page 4
+        // Missing K on Page 4
+        lotrCharacterProviderPage4.Lists.L.Html;
+        lotrCharacterProviderPage4.Lists.M2.Html;
+        lotrCharacterProviderPage4.Lists.``M cont.``.Html;
+        lotrCharacterProviderPage4.Lists.N.Html;
+        lotrCharacterProviderPage4.Lists.O.Html;
+        lotrCharacterProviderPage4.Lists.P2.Html;
+        lotrCharacterProviderPage4.Lists.R.Html;
+        lotrCharacterProviderPage4.Lists.``R cont.``.Html;
+        lotrCharacterProviderPage4.Lists.S2.Html;
+        lotrCharacterProviderPage4.Lists.T2.Html;
+
+        // Page 5
+        lotrCharacterProviderPage5.Lists.T2.Html;
+        lotrCharacterProviderPage5.Lists.``T cont.``.Html;
+        lotrCharacterProviderPage5.Lists.U.Html;
+        lotrCharacterProviderPage5.Lists.V2.Html;
+        lotrCharacterProviderPage5.Lists.W.Html;
+        lotrCharacterProviderPage5.Lists.Y.Html;
+        // Missing Z on Page 5
+        // Missing Æ on Page 5
+        lotrCharacterProviderPage5.Lists.É.Html;
+        lotrCharacterProviderPage5.Lists.Í.Html;
+        // Missing Ó on Page 5
+        lotrCharacterProviderPage5.Lists.Ó.Html;
+    ]
+
+let listOfLinksOfCharacters = List.concat ( getAnchorTagDecendants ( validCharacterLists ))
