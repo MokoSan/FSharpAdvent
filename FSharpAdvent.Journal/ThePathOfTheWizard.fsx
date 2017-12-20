9684a318d582e02990746564a2deaeaf57d22fac
Mukund Raghav Sharma <mukund.sharma92@gmail.com>
2017-12-19 22:39:10 -0500

    Path of the Wizard Complete

--- FSharpAdvent.Journal/ThePathOfTheWizard.fsx
+++ FSharpAdvent.Journal/ThePathOfTheWizard.fsx
@@ -121,13 +121,40 @@ let filteredCharacterDf : Frame < int, string > =
     lotrCharacterDf
     |> Frame.filterRowValues ( fun r -> r.GetAs< string >( "Race" ) <> "Maiar" )
 
-let inputs : string[] =
+#r @"C:\Users\MukundRaghavSharma\Desktop\F#\FSharpAdvent\packages\Accord.3.8.0\lib\net45\Accord.dll"
+#r @"C:\Users\MukundRaghavSharma\Desktop\F#\FSharpAdvent\packages\Accord.MachineLearning.3.8.0\lib\net45\Accord.MachineLearning.dll"
+#r @"C:\Users\MukundRaghavSharma\Desktop\F#\FSharpAdvent\packages\Accord.Math.3.8.0\lib\net45\Accord.Math.dll"
+#r @"C:\Users\MukundRaghavSharma\Desktop\F#\FSharpAdvent\packages\Accord.Math.3.8.0\lib\net45\Accord.Math.Core.dll"
+#r @"C:\Users\MukundRaghavSharma\Desktop\F#\FSharpAdvent\packages\Accord.Statistics.3.8.0\lib\net45\Accord.Statistics.dll"
+
+open System
+
+// Data Partitioning
+
+open RDotNet
+open RProvider
+open RProvider.``base``
+open RProvider.utils
+
+R.install_packages("caret")
+
+open RProvider.caret
+
+let range = [ 1..filteredCharacterDf.Rows.KeyCount ]
+
+let trainingIdx : int[] = R.sample( range, float( range.Length ) * 0.7 ).GetValue()
+let testingIdx  : int[] = R.setdiff( range, trainingIdx ).GetValue()
+
+let trainingData : Frame< int, string > = filteredCharacterDf.Rows.[ trainingIdx ] 
+let testingData  : Frame< int, string > = filteredCharacterDf.Rows.[ testingIdx ]
+
+let getInputs ( dataSet : Frame< int, string > ): string[] =
     filteredCharacterDf
     |> Frame.getCol "FirstName"
     |> Series.values
     |> Seq.toArray
 
-let outputs : int[] = 
+let getOutputs ( dataSet : Frame< int, string > ) : int[] = 
     filteredCharacterDf
     |> Frame.getCol "Race"
     |> Series.values
@@ -140,84 +167,30 @@ let outputs : int[] =
         | "Dwarf"  -> 3
     )
 
-let inputsAndOutputsCombined = 
-    Array.zip inputs outputs
-
 let raceCount : int = races.Length 
 
-#r @"C:\Users\MukundRaghavSharma\Desktop\F#\FSharpAdvent\packages\Accord.3.8.0\lib\net45\Accord.dll"
-#r @"C:\Users\MukundRaghavSharma\Desktop\F#\FSharpAdvent\packages\Accord.MachineLearning.3.8.0\lib\net45\Accord.MachineLearning.dll"
-#r @"C:\Users\MukundRaghavSharma\Desktop\F#\FSharpAdvent\packages\Accord.Math.3.8.0\lib\net45\Accord.Math.dll"
-#r @"C:\Users\MukundRaghavSharma\Desktop\F#\FSharpAdvent\packages\Accord.Math.3.8.0\lib\net45\Accord.Math.Core.dll"
-#r @"C:\Users\MukundRaghavSharma\Desktop\F#\FSharpAdvent\packages\Accord.Statistics.3.8.0\lib\net45\Accord.Statistics.dll"
+open System
 
 open Accord.Math.Distances
 open Accord.MachineLearning
 open Accord.Statistics.Analysis
 
-open System
-open MathNet.Numerics.Random
-
-// Data Partitioning
-
-[<Literal>]
-let trainingDataSplit = 0.8
-
-// Splitting Training and Testing data set
-let totalNumberOfDataPoints = inputs.Length 
-
-let combinedDataIdx =  
-    let random = Random.doublesSeed 100 totalNumberOfDataPoints
-    let training =
-        random
-        |> Array.mapi( fun i r -> i, r < trainingDataSplit )
-
-    let testing = 
-        random
-        |> Array.mapi( fun i r -> i, r > trainingDataSplit )
-
-    training, testing 
-
-let trainingData =
-    let getAllFilteredTrainingData =
-        ( fst combinedDataIdx )
-        |> Array.filter( fun ( r,i ) -> ( i ))
-        |> Array.map( fun ( r, i ) -> r ) 
-
-    getAllFilteredTrainingData
-    |> Array.map( fun r -> inputsAndOutputsCombined.[ r ])
-
-let testingData = 
-    let getAllFilteredTestingData =
-        ( snd combinedDataIdx )
-        |> Array.filter( fun ( r,i ) -> ( i ))
-        |> Array.map( fun ( r, i ) -> r ) 
-
-    getAllFilteredTestingData 
-    |> Array.map( fun r -> inputsAndOutputsCombined.[ r ])
-
-let trainingDataInput =
-    trainingData
-    |> Array.map( fun d -> fst d )
-
-let trainingDataOutput =
-    trainingData
-    |> Array.map( fun d -> snd d )
-
-let testingDataInput =
-    trainingData
-    |> Array.map( fun d -> fst d )
-
-let testingDataOutput =  
-    trainingData
-    |> Array.map( fun d -> snd d )
-    
 // Learning phase should be based on the Training Data 
-let knn           = KNearestNeighbors< string >( k = raceCount, distance = Levenshtein() )
-let knnLearnt     = knn.Learn( trainingDataInput, trainingDataOutput )
+let trainingDataInputs  : string[] = getInputs( trainingData ) 
+let trainingDataOutputs : int[]    = getOutputs( trainingData ) 
 
-let classificationResultWrapper ( name : string ) 
-                                ( knn : KNearestNeighbors< string > ) : string = 
+// Function that trains the Model based on the 'k', the number of nearest neighbors
+// to consider to classify a new data point.
+let train ( k : int ) : KNearestNeighbors< string > = 
+    let knn : KNearestNeighbors< string > = 
+        KNearestNeighbors< string >( k = k, distance = Levenshtein() )
+
+    knn.Learn( trainingDataInputs, trainingDataOutputs )
+
+//let k : int = int ( Math.Ceiling ( Math.Sqrt( float( trainingDataInputs.Length ) / 2.0 )))
+
+let predict ( knn : KNearestNeighbors< string > ) 
+            ( name : string ) : string = 
     let decidedClass = knn.Decide( name )
     match decidedClass with
         | 0 -> "Human" 
@@ -226,13 +199,68 @@ let classificationResultWrapper ( name : string )
         | 3 -> "Dwarf"
         | _ -> failwith "Class not found"
 
-classificationResultWrapper "Arwe" knnLearnt
+let modelWithK5 = train 5
 
-// Confusion Matrix should be based on the Testing Data 
-let confusionMatrix = GeneralConfusionMatrix.Estimate( knnLearnt, testingDataInput, testingDataOutput )
+predict modelWithK5 "Moko" 
+predict modelWithK5 "Bori" 
+predict modelWithK5 "Terry" 
+
+// Cross Validation should be based on the Testing Data 
+let testingDataInputs  : string[] = getInputs( testingData ) 
+let testingDataOutputs : int[]    = getOutputs( testingData )
+
+let knnWith5Ks      = train( 5 )
+let confusionMatrix = 
+    GeneralConfusionMatrix.Estimate( knnWith5Ks, 
+                                     testingDataInputs, 
+                                     testingDataOutputs )
 let accuracy        = confusionMatrix.Accuracy
 let error           = confusionMatrix.Error
-let kappa           = confusionMatrix.Kappa
+
+let crossValidationBasedOnK ( k : int ) : GeneralConfusionMatrix = 
+    let knnModel        : KNearestNeighbors< string > 
+        = train( k )
+    GeneralConfusionMatrix.Estimate( knnModel, testingDataInputs, testingDataOutputs )
+
+let rangeOfKs : int list  = [ 1..20 ]
+let crossValidationResults : GeneralConfusionMatrix list = 
+    rangeOfKs
+    |> List.map( crossValidationBasedOnK )
+
+let listOfAccuracies : float list = 
+    crossValidationResults
+    |> List.map( fun c -> c.Accuracy * 100.0 )
+
+let listOfErrors : float list = 
+    crossValidationResults
+    |> List.map( fun c -> c.Error * 100.0 )
+
+// Let's chart.
+let ksToAccuracies : Frame< string, int > = 
+    List.zip rangeOfKs listOfAccuracies 
+    |> List.map( fun ( k, a ) -> "K", k, a )
+    |> Frame.ofValues
+
+ksToAccuracies
+|> Chart.Column
+|> Chart.WithTitle "Accuracies vs. K"
+|> Chart.WithYTitle "Accuracy [ % ]"
+|> Chart.WithLegend true
+|> Chart.Show
+
+let ksToErrors : Frame< string, int > = 
+    List.zip rangeOfKs listOfErrors 
+    |> List.map( fun ( k, e) -> "K", k, e )
+    |> Frame.ofValues
+
+ksToErrors
+|> Chart.Column
+|> Chart.WithTitle "Errors vs. K"
+|> Chart.WithYTitle "Error [ % ]"
+|> Chart.WithLegend true
+|> Chart.Show
+
+
 
 (**
 Summary
